52ed2bbfe6479bdc1e9acecf3a23321efcf4779c
arvindeva <arvindeva@gmail.com>
2019-06-23 13:15:10 +0700

    Add SEO keywords

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,26 @@ import Home from '../components/Home';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" description="arvindeva wibisono"/>
+    <SEO
+      title="Home"
+      description="arvindeva wibisono"
+      keywords={[
+        `arvindeva wibisono`,
+        `arvindeva`,
+        `wibisono`,
+        `front end`,
+        `react`,
+        `engineer`,
+        `software`,
+        `developer`,
+        `gatsby`,
+        `warung pintar`,
+        `jakarta`,
+        `ade`,
+        `arvin`,
+        `dev`,
+      ]}
+    />
     <Home />
   </Layout>
 );
